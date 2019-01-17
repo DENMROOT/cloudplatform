243b2797d5a9b24750db1f96871544418c7c7edf
Denys Makarov <denm2000@gmail.com>
2019-01-17 13:28:01 +0200

    Initial commit

---
+++ src/main/java/com/exonum/test/cloudplatform/CloudplatformApplication.java
@@ -0,0 +1,15 @@
+package com.exonum.test.cloudplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudplatformApplication.class, args);
+	}
+
+}
+
